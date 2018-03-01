b032b18cc73261e5478abd969cf9c24439d8724a
Raghu Betina <raghu@firstdraft.com>
2018-03-01 06:04:39 +0000

    3rd draft

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -1,7 +1,6 @@
-ActiveAdmin.register FriendRequest do
+ActiveAdmin.register Photo do
 
- permit_params :sender_id, :recipient_id, :status
-
+ permit_params :caption, :image, :location, :owner_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -14,6 +13,5 @@ ActiveAdmin.register FriendRequest do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
 
 end
--- app/admin/votes.rb
+++ app/admin/votes.rb
@@ -1,7 +1,6 @@
-ActiveAdmin.register Photo do
+ActiveAdmin.register Vote do
 
- permit_params :caption, :image, :location, :owner_id
-
+ permit_params :user_id, :photo_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -14,6 +13,5 @@ ActiveAdmin.register Photo do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
 
 end
